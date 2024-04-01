c82e8ec2362c9e5c4169d3897481f2d07066f1c0
MiroTragico12 <jorge.santista10@hotmail.com>
2024-04-01 13:22:11 -0300

    tratamento de excessao do update

--- src/main/java/com/zaninweb/course/services/UserService.java
+++ src/main/java/com/zaninweb/course/services/UserService.java
@@ -5,13 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.zaninweb.course.entities.User;
 import com.zaninweb.course.repositories.UserRepository;
 import com.zaninweb.course.services.exceptions.DatabaseException;
 import com.zaninweb.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -40,16 +41,20 @@ public class UserService {
 			repository.deleteById(id);
 		} catch (ResourceNotFoundException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+	}catch (EntityNotFoundException e) {
+		throw new ResourceNotFoundException(id);
 	}
+		}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
